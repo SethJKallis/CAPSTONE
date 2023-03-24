79ef8e9144db33dc7d0f87ab15b906284ba7e0c2
SethJKallis <kallisseth@gmail.com>
2023-03-24 20:28:35 +0200

    added firebase link

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://capstone-final-711e0.web.app/');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Headers', '*');
